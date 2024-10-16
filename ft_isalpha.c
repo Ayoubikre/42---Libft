182e642e1a42aef40ce2e0f554571bd8bc1cd92c
ayoubikr <ayoubikrr@gmail.com>
2024-10-16 09:24:12 +0100

    add

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -5,16 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Noctis <Noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/24 02:03:18 by Noctis            #+#    #+#             */
-/*   Updated: 2024/09/24 02:31:59 by Noctis           ###   ########.fr       */
+/*   Created: 2024/10/16 09:19:06 by Noctis            #+#    #+#             */
+/*   Updated: 2024/10/16 09:20:08 by Noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_isalpha(int c)
-{
-	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-		return (1);
-	return (0);
-}
+#include
