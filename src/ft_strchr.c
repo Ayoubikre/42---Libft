c02ea318f22b9eeec7f48b858d301b16f9760358
Ayoub Akritah <aakritah@c2r1p7.1337.ma>
2024-10-25 21:49:30 +0100

    add

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,19 +6,24 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:32:59 by aakritah          #+#    #+#             */
-/*   Updated: 2024/10/25 19:27:33 by aakritah         ###   ########.fr       */
+/*   Updated: 2024/10/25 21:46:31 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char *ft_strchr(const char *s, int c)
 {
-	while (*s)
+	size_t s1;
+	size_t i;
+
+	s1 = ft_strlen((char *)s);
+	i = 0;
+	while (i < s1 + 1)
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
 	}
-	return ((char *)s);
+	return (NULL);
 }
